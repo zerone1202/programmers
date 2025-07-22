54483544329580feaff34c3f918ed71c7613b71c
zerone <bazz0324@naver.com>
2025-07-22 22:06:00 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 1;
+
+    if(num1 == num2){
+        return answer;
+    } else {
+      return answer -2;   
+    }
+}
